9985581a636015e9083449f306750e97128724d6
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2014-12-16 19:16:38 -0200

    Updated Options after refactoring.

--- lib/gordon/options.rb
+++ lib/gordon/options.rb
@@ -1,10 +1,11 @@
 module Gordon
   class Options
     attr_accessor :app_type, :app_name, :app_desc, :app_repo, :app_version
-    attr_accessor :package_type, :package_dir
-    attr_accessor :source_dir, :build_dir
-    attr_accessor :skeleton_type
-    attr_accessor :init_type, :init_build_dir
+    attr_accessor :package_type
+    attr_accessor :source_dir
+    attr_accessor :output_dir
+    attr_accessor :http_server_type
+    attr_accessor :init_type
     attr_accessor :debug
 
     def debug? ; !!debug ; end
