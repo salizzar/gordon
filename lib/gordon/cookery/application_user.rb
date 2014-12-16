0999243a716ef5a709e1f5f708846f068b4e8fa8
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2014-12-16 19:39:23 -0200

    Created module Cookery::ApplicationUser to handle user creation and /home/oem ownership.

---
+++ lib/gordon/cookery/application_user.rb
@@ -0,0 +1,41 @@
+module Gordon
+  module Cookery
+    module ApplicationUser
+      def create_user_and_group(home_path)
+        File.open(builddir('before-install'), 'w', 0755) do |f|
+          bash = <<-__BASH
+#!/bin/sh
+
+set -e
+
+/usr/bin/getent group  #{$env_vars.app_name} >/dev/null || /usr/sbin/groupadd --system #{$env_vars.app_name};
+/usr/bin/getent passwd #{$env_vars.app_name} >/dev/null || /usr/sbin/useradd  --system --gid #{$env_vars.app_name} --home-dir #{home_path} --shell /sbin/nologin --comment "#{$env_vars.app_desc}" #{$env_vars.app_name} >/dev/null || :;
+
+              __BASH
+
+          f.write(bash)
+
+          self.class.pre_install(File.expand_path(f.path))
+        end
+      end
+
+      def setup_user_permissions(home_path)
+        File.open(builddir('after-install'), 'w', 0755) do |f|
+          bash = <<-__BASH
+#!/bin/sh
+
+set -e
+
+/usr/bin/chown -R #{$env_vars.app_name}:#{$env_vars.app_name} #{home_path}
+
+              __BASH
+
+          f.write(bash)
+
+          self.class.post_install(File.expand_path(f.path))
+        end
+      end
+    end
+  end
+end
+
