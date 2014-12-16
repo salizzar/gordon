b2da7f8121dfc4ae21144e162e848b2b525533ff
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2014-12-16 19:20:19 -0200

    Created Cookery::HttpServer to handle application files installation.

---
+++ lib/gordon/cookery/http_server.rb
@@ -0,0 +1,14 @@
+module Gordon
+  module Cookery
+    module HttpServer
+      def install_http_server_files(blacklist)
+        skeleton_path = get_skeleton_path_from_type($env_vars.http_server_type)
+
+        application_files = all_files_except_blacklisted(blacklist)
+
+        root(skeleton_path).install application_files
+      end
+    end
+  end
+end
+
