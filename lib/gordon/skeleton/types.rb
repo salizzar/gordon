26bd96c5579a050f9d89b4bd24660e5bffaa23e7
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-01-03 00:35:35 -0200

    Updated Gordon::Skeleton::Types to map OS package name for CentOS and Debian.

--- lib/gordon/skeleton/types.rb
+++ lib/gordon/skeleton/types.rb
@@ -14,29 +14,35 @@ module Gordon
       class Nginx
         include Base
 
+        def get_os_package_map ; { centos: :nginx, debian: :nginx } ; end
+
         def get_default_path ; '/usr/share/nginx/html' ; end
       end
 
-      class Httpd
+      class Apache
         include Base
+
+        def get_os_package_map ; { centos: :httpd, debian: :apache2 } ; end
 
         def get_default_path ; '/var/www/html' ; end
       end
 
-      class Misc
+      class Systemd
         include Base
 
-        def get_default_path ; '/opt' ; end
+        def get_os_package_map ; { centos: :systemd, debian: :systemd } ; end
+
+        def get_default_path ; '/usr/lib/systemd/system' ; end
 
         def requires_app_name?
           true
         end
       end
 
-      class Systemd
+      class Misc
         include Base
 
-        def get_default_path ; '/usr/lib/systemd/system' ; end
+        def get_default_path ; '/opt' ; end
 
         def requires_app_name?
           true
