48f83f08f5bc949ef21bb6637336e98d7108c722
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-01-03 00:37:12 -0200

    Updated Gordon::Options to handle source_dir as app_source_dir.

--- lib/gordon/options.rb
+++ lib/gordon/options.rb
@@ -1,11 +1,11 @@
 module Gordon
   class Options
-    attr_accessor :app_type, :app_name, :app_desc, :app_repo, :app_version
-    attr_accessor :package_type
-    attr_accessor :source_dir
-    attr_accessor :output_dir
+    attr_accessor :app_type, :app_name, :app_desc, :app_repo, :app_version, :app_source_dir
+    attr_accessor :runtime_version
     attr_accessor :http_server_type
     attr_accessor :init_type
+    attr_accessor :package_type
+    attr_accessor :output_dir
     attr_accessor :debug
 
     def debug? ; !!debug ; end
