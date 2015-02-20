838c74089c07b99f859d9ce78bdf6e1d743f7ce5
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-20 13:50:13 -0200

    Added entry for Tomcat and Jetty skeleton types spec.

--- spec/gordon/skeleton/types_spec.rb
+++ spec/gordon/skeleton/types_spec.rb
@@ -12,6 +12,14 @@ describe Gordon::Skeleton::Types::Apache do
   pending
 end
 
+describe Gordon::Skeleton::Types::Tomcat do
+  pending
+end
+
+describe Gordon::Skeleton::Types::Jetty do
+  pending
+end
+
 describe Gordon::Skeleton::Types::Systemd do
   pending
 end
