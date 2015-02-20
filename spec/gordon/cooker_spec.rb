9d5909789d052002785d5d0362b87bfb7d36d647
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-20 13:48:06 -0200

    Fixed broken pending on Gordon::Cooker spec.

--- spec/gordon/cooker_spec.rb
+++ spec/gordon/cooker_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe Gordon::Cooker do
   describe 'cooking a package' do
-    it 'cleans fpm-cookery working directory' do
+    describe 'cleans fpm-cookery working directory' do
       pending
     end
 
-    it 'packages calling fpm-cookery' do
+    describe 'packages calling fpm-cookery' do
       pending
     end
   end
