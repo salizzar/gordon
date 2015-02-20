3c369a027dd285eaac90b02c68d5287bcbce6a91
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-20 13:46:48 -0200

    Added web_server_type attribute in Gordon::Options.

--- lib/gordon/options.rb
+++ lib/gordon/options.rb
@@ -3,6 +3,7 @@ module Gordon
     attr_accessor :app_type, :app_name, :app_desc, :app_repo, :app_version, :app_source_dir
     attr_accessor :runtime_version
     attr_accessor :http_server_type
+    attr_accessor :web_server_type
     attr_accessor :init_type
     attr_accessor :package_type
     attr_accessor :output_dir
