044e7c3de0e362f02a29cd02f67cb5409178f844
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-24 21:20:48 -0300

    Created specs for Gordon::Process.

--- lib/gordon/process.rb
+++ lib/gordon/process.rb
@@ -2,7 +2,11 @@ module Gordon
   class Process
     def self.run(command)
       pid = ::Process.spawn(command, out: $stdout, err: $stderr)
-      _, status = ::Process.wait2(pid)
+      _, exit_status = ::Process.wait2(pid)
+
+      raise SystemCallError.new("Failed to execute [#{command}]. Exit code: #{exit_status}") unless exit_status == 0
+
+      true
     end
   end
 end
--- spec/gordon/process_spec.rb
+++ spec/gordon/process_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 
 describe Gordon::Process do
+  let(:command) { 'uname -a' }
+  let(:pid)     { 3329 }
+
   describe 'running a command' do
-    pending
+    before :each do
+      expect(Process).to receive(:spawn).with(command, out: $stdout, err: $stderr).and_return(pid)
+    end
+
+    it 'spawns and waits for execution' do
+      expect(Process).to receive(:wait2).with(pid).and_return([ pid, 0 ])
+
+      expect(described_class.run(command)).to be_truthy
+    end
+
+    it 'raises error if call returns one' do
+      expect(Process).to receive(:wait2).with(pid).and_return([ pid, 1 ])
+
+      expect{ described_class.run(command) }.to raise_error
+    end
   end
 end
 
