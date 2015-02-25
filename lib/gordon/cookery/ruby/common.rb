babe3818bc5fcecec671bdd254332ac3113f41d0
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-24 21:16:09 -0300

    Created specs for Gordon::Cookery::Ruby::Common.

--- lib/gordon/cookery/ruby/common.rb
+++ lib/gordon/cookery/ruby/common.rb
@@ -3,14 +3,6 @@ module Gordon
     module Ruby
       module Common
         RUBY_BLACKLIST_FILES = %w(.rspec coverage log spec tmp)
-
-        def ruby_vendor_gems
-          command = 'ruby -S bundle package --all'
-          safesystem(command)
-
-          command = 'ruby -S bundle install --deployment --without development test'
-          safesystem(command)
-        end
       end
     end
   end
--- spec/gordon/cookery/ruby/common_spec.rb
+++ spec/gordon/cookery/ruby/common_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe Gordon::Cookery::Ruby::Common do
-  describe 'vendorizing gems' do
-    pending
+  describe 'default blacklist' do
+    it 'maps common files' do
+      expected = %w(.rspec coverage log spec tmp)
+
+      expect(described_class::RUBY_BLACKLIST_FILES).to eq(expected)
+    end
   end
 end
 
