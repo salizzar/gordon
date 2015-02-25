2e5b15fae67e228ac29cb5ad3856093154daeac8
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-24 21:21:43 -0300

    Created specs for Gordon::Factory.

--- spec/gordon/factory_spec.rb
+++ spec/gordon/factory_spec.rb
@@ -2,7 +2,21 @@ require 'spec_helper'
 
 describe Gordon::Factory do
   describe 'creating a instance of some object' do
-    pending
+    it 'returns a new instance of a skeleton' do
+      namespace, object_type = "Skeleton::Types", "systemd"
+
+      result = described_class.create_instance(namespace, object_type)
+
+      expect(result).to be_instance_of(Gordon::Skeleton::Types::Systemd)
+    end
+
+    it 'returns a new instance of a application type' do
+      namespace, object_type = "Application::Types", "ruby_web_app"
+
+      result = described_class.create_instance(namespace, object_type)
+
+      expect(result).to be_instance_of(Gordon::Application::Types::RubyWebApp)
+    end
   end
 end
 
