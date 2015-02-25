5b6beb0e17c4818ff182794a5f7ed7d456cb222f
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-24 21:25:10 -0300

    Updated Gordon::EnvVars to handle parameter runtime_name.

--- lib/gordon/env_vars.rb
+++ lib/gordon/env_vars.rb
@@ -1,7 +1,8 @@
 module Gordon
   class EnvVars
-    attr_accessor :app_type, :app_name, :app_desc, :app_repo, :app_version, :app_source_dir
-    attr_accessor :runtime_version
+    attr_accessor :app_type
+    attr_accessor :app_name, :app_description, :app_homepage, :app_version, :app_source
+    attr_accessor :runtime_name, :runtime_version
     attr_accessor :http_server_type
     attr_accessor :web_server_type
     attr_accessor :init_type
@@ -11,10 +12,11 @@ module Gordon
 
       env_vars << "GORDON_APP_TYPE='#{options.app_type}'"
       env_vars << "GORDON_APP_NAME='#{options.app_name}'"
-      env_vars << "GORDON_APP_DESC='#{options.app_desc}'"
-      env_vars << "GORDON_APP_REPO='#{options.app_repo}'"
+      env_vars << "GORDON_APP_DESCRIPTION='#{options.app_description}'"
+      env_vars << "GORDON_APP_HOMEPAGE='#{options.app_homepage}'"
       env_vars << "GORDON_APP_VERSION='#{options.app_version}'"
-      env_vars << "GORDON_APP_SOURCE_DIR='#{File.expand_path(options.app_source_dir)}'"
+      env_vars << "GORDON_APP_SOURCE='#{File.expand_path(options.app_source)}'"
+      env_vars << "GORDON_RUNTIME_NAME='#{options.runtime_name}'"
       env_vars << "GORDON_RUNTIME_VERSION='#{options.runtime_version}'"
       env_vars << "GORDON_HTTP_SERVER_TYPE='#{options.http_server_type}'"
       env_vars << "GORDON_WEB_SERVER_TYPE='#{options.web_server_type}'"
@@ -28,10 +30,11 @@ module Gordon
 
       env_vars.app_type         = ENV['GORDON_APP_TYPE']
       env_vars.app_name         = ENV['GORDON_APP_NAME']
-      env_vars.app_desc         = ENV['GORDON_APP_DESC']
-      env_vars.app_repo         = ENV['GORDON_APP_REPO']
+      env_vars.app_description  = ENV['GORDON_APP_DESCRIPTION']
+      env_vars.app_homepage     = ENV['GORDON_APP_HOMEPAGE']
       env_vars.app_version      = ENV['GORDON_APP_VERSION']
-      env_vars.app_source_dir   = ENV['GORDON_APP_SOURCE_DIR']
+      env_vars.app_source       = ENV['GORDON_APP_SOURCE']
+      env_vars.runtime_name     = ENV['GORDON_RUNTIME_NAME']
       env_vars.runtime_version  = ENV['GORDON_RUNTIME_VERSION']
       env_vars.http_server_type = ENV['GORDON_HTTP_SERVER_TYPE']
       env_vars.web_server_type  = ENV['GORDON_WEB_SERVER_TYPE']
