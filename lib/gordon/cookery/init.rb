3fa9db656cbd9f609b9773885a9d512684c24675
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-02-24 21:17:17 -0300

    Created specs for Gordon::Cookery::Init.

--- lib/gordon/cookery/init.rb
+++ lib/gordon/cookery/init.rb
@@ -1,11 +1,13 @@
 module Gordon
   module Cookery
     module Init
+      include Common
+
       def create_init(env_vars, skeleton_type)
         init_build_dir_path = builddir(env_vars.init_type)
-        skeleton_path = get_skeleton_path_from_type(skeleton_type)
+        skeleton_path = get_skeleton_path_from_type(env_vars, skeleton_type)
 
-        command = "ruby -S foreman export --procfile Procfile --root #{skeleton_path} --app #{env_vars.app_name} --user #{env_vars.app_name} #{env_vars.init_type} #{init_build_dir_path}"
+        command = "foreman export --procfile Procfile --root #{skeleton_path} --app #{env_vars.app_name} --user #{env_vars.app_name} #{env_vars.init_type} #{init_build_dir_path}"
 
         safesystem(command)
       end
@@ -13,7 +15,7 @@ module Gordon
       def install_init(env_vars)
         init_build_dir_path = builddir(env_vars.init_type)
 
-        skeleton_path = get_skeleton_path_from_type(env_vars.init_type)
+        skeleton_path = get_skeleton_path_from_type(env_vars, env_vars.init_type)
         skeleton_files = Dir["#{init_build_dir_path}/*"]
 
         root(skeleton_path).install skeleton_files
--- spec/gordon/cookery/init_spec.rb
+++ spec/gordon/cookery/init_spec.rb
@@ -1,11 +1,61 @@
 require 'spec_helper'
 
 describe Gordon::Cookery::Init do
+  subject do
+    class MyInit
+      include Gordon::Cookery::Init
+    end.new
+  end
+
+  let(:app_name)    { "gordon" }
+  let(:init_type)   { :systemd }
+  let(:build_path)  { '/tmp/gordon/tmp-build/systemd' }
+  let(:skeleton)    { Gordon::Skeleton::Types::Systemd.new }
+  let(:env_vars)    { instance_double Gordon::EnvVars, app_name: app_name, init_type: init_type }
+
+  before :each do
+    expect(subject).to receive(:builddir).with(env_vars.init_type).and_return(build_path)
+
+    expect(Gordon::Skeleton::Types::Systemd).to receive(:new).and_return(skeleton)
+  end
+
   describe 'creating init files' do
-    pending
+    it 'creates init calling foreman' do
+      command = %W{
+        foreman
+        export
+        --procfile
+        Procfile
+        --root
+        #{skeleton.path}
+        --app
+        #{env_vars.app_name}
+        --user
+        #{env_vars.app_name}
+        #{env_vars.init_type}
+        #{build_path}
+      }
+
+      expect(subject).to receive(:safesystem).with(command.join " ")
+
+      subject.create_init(env_vars, init_type)
+    end
   end
 
   describe 'installing init files' do
-    pending
+    it 'installs all generated files' do
+      files = %w(app.target app.service app-web-1.service).map do |file|
+        File.join(build_path, file)
+      end
+
+      expect(Dir).to receive(:[]).with("#{build_path}/*").and_return(files)
+
+      installer = double 'a path helper'
+      expect(subject).to receive(:root).with(skeleton.path).and_return(installer)
+      expect(installer).to receive(:install).with(files)
+
+      subject.install_init(env_vars)
+    end
   end
 end
+
