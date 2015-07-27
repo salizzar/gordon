a6b37567b853ed722d432177a313fdff9eec2a6b
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-07-27 17:25:01 -0300

    Created app_source_excludes parameter on Gordon::Options.

--- lib/gordon/options.rb
+++ lib/gordon/options.rb
@@ -1,7 +1,6 @@
 module Gordon
   class Options
-    attr_accessor :app_name, :app_description, :app_homepage, :app_version, :app_source
-    attr_accessor :app_type
+    attr_accessor :app_name, :app_description, :app_homepage, :app_version, :app_source, :app_source_excludes, :app_type
     attr_accessor :runtime_name, :runtime_version
     attr_accessor :http_server_type
     attr_accessor :web_server_type
@@ -9,5 +8,32 @@ module Gordon
     attr_accessor :package_type
     attr_accessor :output_dir
     attr_accessor :debug, :trace
+
+    def self.from(main_options, recipe)
+      self.new.tap do |opt|
+        opt.app_type            = main_options.app_type         || recipe['app_type']
+        opt.app_name            = main_options.app_name         || recipe['app_name']
+        opt.app_description     = main_options.app_description  || recipe['app_description']
+        opt.app_homepage        = main_options.app_homepage     || recipe['app_homepage']
+        opt.app_version         = main_options.app_version      || recipe['app_version']
+        opt.app_source          = recipe['app_source']          || main_options.app_source
+
+        opt.app_source_excludes = (main_options.app_source_excludes.to_a + recipe['app_source_excludes'].to_a).flatten
+
+        opt.runtime_name        = main_options.runtime_name         || recipe['runtime_name']
+        opt.runtime_version     = main_options.runtime_version      || recipe['runtime_version']
+
+        opt.http_server_type    = main_options.http_server_type     || recipe['http_server_type']
+        opt.web_server_type     = main_options.web_server_type      || recipe['web_server_type']
+        opt.init_type           = main_options.init_type            || recipe['init_type']
+
+        opt.package_type        = main_options.package_type         || recipe['package_type']
+        opt.output_dir          = main_options.output_dir           || recipe['output_dir']
+
+        opt.debug               = !main_options.debug.nil? ? main_options.debug : recipe['debug']
+        opt.trace               = !main_options.trace.nil? ? main_options.trace : recipe['trace']
+      end
+    end
   end
 end
+
--- spec/gordon/options_spec.rb
+++ spec/gordon/options_spec.rb
@@ -1,8 +1,74 @@
 require 'spec_helper'
 
 describe Gordon::Options do
-  describe 'mapping default accessors' do
-    pending
+  let(:main_option_attributes) do
+    {
+      app_name:             'a-test',
+      app_description:      'a description',
+      app_homepage:         'https://github.com/salizzar/gordon',
+      app_version:          Gordon::VERSION,
+      app_source:           '.',
+      app_source_excludes:  %w(something),
+      app_type:             'web',
+      runtime_name:         'ruby',
+      runtime_version:      '2.2.2',
+      http_server_type:     'nginx',
+      web_server_type:      nil,
+      init_type:            'systemd',
+      package_type:         'rpm',
+      output_dir:           'pkg',
+      debug:                false,
+      trace:                true,
+    }
+  end
+
+  let(:recipe) do
+    {
+      'app_name'            => 'xirubiru',
+      'app_description'     => 'another description',
+      'app_homepage'        => 'https://github.com/salizzar/xirubiru',
+      'app_version'         => '1.0.1',
+      'app_source'          => '/tmp',
+      'app_source_excludes' =>  %w(something),
+      'app_type'            => 'standalone',
+      'runtime_name'        => 'ruby',
+      'runtime_version'     => '2.2.1',
+      'http_server_type'    => nil,
+      'web_server_type'     => nil,
+      'init_type'           => 'systemd',
+      'package_type'        => 'rpm',
+      'output_dir'          => 'pkg',
+      'debug'               => true,
+      'trace'               => false,
+    }
+  end
+
+  let(:main_options)    { instance_double(described_class, main_option_attributes) }
+
+  describe 'returning instance based on main options and a recipe' do
+    it 'priorizes main options instead of recipe' do
+      instance = described_class.from(main_options, recipe)
+
+      main_option_attributes.delete_if do |(k, _)|
+        [ :app_source, :app_source_excludes ].include?(k)
+      end.each do |(k, v)|
+        expect(instance.send(k)).to eq(v)
+      end
+    end
+
+    it 'priorizes app source from recipe instead of main option' do
+      instance = described_class.from(main_options, recipe)
+
+      expect(instance.app_source).to eq(recipe['app_source'])
+    end
+
+    it 'merges app source excludes values from main options and recipes' do
+      instance = described_class.from(main_options, recipe)
+
+      expected = (main_option_attributes[:app_source_excludes].to_a + recipe['app_source_excludes'].to_a).flatten
+
+      expect(instance.app_source_excludes).to eq(expected)
+    end
   end
 end
 
