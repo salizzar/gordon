d7935fe2a294880b8c53b1586555c10819dfdab3
Marcelo Pinheiro <marcelo.pinheiro@moip.com.br>
2015-07-27 17:25:43 -0300

    Updated Gordon::EnvVars to handle app_source_excludes parameter.

--- lib/gordon/env_vars.rb
+++ lib/gordon/env_vars.rb
@@ -1,7 +1,6 @@
 module Gordon
   class EnvVars
-    attr_accessor :app_type
-    attr_accessor :app_name, :app_description, :app_homepage, :app_version, :app_source
+    attr_accessor :app_name, :app_description, :app_homepage, :app_version, :app_source, :app_source_excludes, :app_type
     attr_accessor :runtime_name, :runtime_version
     attr_accessor :http_server_type
     attr_accessor :web_server_type
@@ -9,13 +8,13 @@ module Gordon
 
     def self.from_cook(options)
       env_vars = []
-
       env_vars << "GORDON_APP_TYPE='#{options.app_type}'"
       env_vars << "GORDON_APP_NAME='#{options.app_name}'"
       env_vars << "GORDON_APP_DESCRIPTION='#{options.app_description}'"
       env_vars << "GORDON_APP_HOMEPAGE='#{options.app_homepage}'"
       env_vars << "GORDON_APP_VERSION='#{options.app_version}'"
       env_vars << "GORDON_APP_SOURCE='#{File.expand_path(options.app_source)}'"
+      env_vars << "GORDON_APP_SOURCE_EXCLUDES='#{options.app_source_excludes.to_a.map { |path| File.expand_path(path)}.join(",") }'"
       env_vars << "GORDON_RUNTIME_NAME='#{options.runtime_name}'"
       env_vars << "GORDON_RUNTIME_VERSION='#{options.runtime_version}'"
       env_vars << "GORDON_HTTP_SERVER_TYPE='#{options.http_server_type}'"
@@ -28,17 +27,18 @@ module Gordon
     def self.load
       env_vars = self.new
 
-      env_vars.app_type         = ENV['GORDON_APP_TYPE']
-      env_vars.app_name         = ENV['GORDON_APP_NAME']
-      env_vars.app_description  = ENV['GORDON_APP_DESCRIPTION']
-      env_vars.app_homepage     = ENV['GORDON_APP_HOMEPAGE']
-      env_vars.app_version      = ENV['GORDON_APP_VERSION']
-      env_vars.app_source       = ENV['GORDON_APP_SOURCE']
-      env_vars.runtime_name     = ENV['GORDON_RUNTIME_NAME']
-      env_vars.runtime_version  = ENV['GORDON_RUNTIME_VERSION']
-      env_vars.http_server_type = ENV['GORDON_HTTP_SERVER_TYPE']
-      env_vars.web_server_type  = ENV['GORDON_WEB_SERVER_TYPE']
-      env_vars.init_type        = ENV['GORDON_INIT_TYPE']
+      env_vars.app_type             = ENV['GORDON_APP_TYPE']
+      env_vars.app_name             = ENV['GORDON_APP_NAME']
+      env_vars.app_description      = ENV['GORDON_APP_DESCRIPTION']
+      env_vars.app_homepage         = ENV['GORDON_APP_HOMEPAGE']
+      env_vars.app_version          = ENV['GORDON_APP_VERSION']
+      env_vars.app_source           = ENV['GORDON_APP_SOURCE']
+      env_vars.app_source_excludes  = ENV['GORDON_APP_SOURCE_EXCLUDES'].to_s.split(",")
+      env_vars.runtime_name         = ENV['GORDON_RUNTIME_NAME']
+      env_vars.runtime_version      = ENV['GORDON_RUNTIME_VERSION']
+      env_vars.http_server_type     = ENV['GORDON_HTTP_SERVER_TYPE']
+      env_vars.web_server_type      = ENV['GORDON_WEB_SERVER_TYPE']
+      env_vars.init_type            = ENV['GORDON_INIT_TYPE']
 
       env_vars
     end
--- spec/gordon/env_vars_spec.rb
+++ spec/gordon/env_vars_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 describe Gordon::EnvVars do
   let(:attributes) do
     {
-      app_type:         "web",
-      app_name:         "xirubiru",
-      app_description:  "a nice description",
-      app_homepage:     "http://some.github.repo",
-      app_version:      "2.0.0",
-      app_source:       ".",
-      runtime_name:     "ruby",
-      runtime_version:  "2.2.0",
-      http_server_type: "nginx",
-      web_server_type:  "tomcat",
-      init_type:        "systemd",
+      app_type:             "web",
+      app_name:             "xirubiru",
+      app_description:      "a nice description",
+      app_homepage:         "http://some.github.repo",
+      app_version:          "2.0.0",
+      app_source:           ".",
+      app_source_excludes:  %w(one two three),
+      runtime_name:         "ruby",
+      runtime_version:      "2.2.0",
+      http_server_type:     "nginx",
+      web_server_type:      "tomcat",
+      init_type:            "systemd",
     }
   end
 
@@ -28,6 +29,7 @@ describe Gordon::EnvVars do
         "GORDON_APP_HOMEPAGE='#{options.app_homepage}'",
         "GORDON_APP_VERSION='#{options.app_version}'",
         "GORDON_APP_SOURCE='#{File.expand_path(options.app_source)}'",
+        "GORDON_APP_SOURCE_EXCLUDES='#{options.app_source_excludes.map{ |path| File.expand_path(path) }.join(",") }'",
         "GORDON_RUNTIME_NAME='#{options.runtime_name}'",
         "GORDON_RUNTIME_VERSION='#{options.runtime_version}'",
         "GORDON_HTTP_SERVER_TYPE='#{options.http_server_type}'",
@@ -42,24 +44,27 @@ describe Gordon::EnvVars do
   describe 'loading from environment' do
     it 'returns a instance with all specified values' do
       env = {
-        'GORDON_APP_TYPE'         => options.app_type,
-        'GORDON_APP_NAME'         => options.app_name,
-        'GORDON_APP_DESCRIPTION'  => options.app_description,
-        'GORDON_APP_HOMEPAGE'     => options.app_homepage,
-        'GORDON_APP_VERSION'      => options.app_version,
-        'GORDON_APP_SOURCE'       => options.app_source,
-        'GORDON_RUNTIME_NAME'     => options.runtime_name,
-        'GORDON_RUNTIME_VERSION'  => options.runtime_version,
-        'GORDON_HTTP_SERVER_TYPE' => options.http_server_type,
-        'GORDON_WEB_SERVER_TYPE'  => options.web_server_type,
-        'GORDON_INIT_TYPE'        => options.init_type,
+        'GORDON_APP_TYPE'             => options.app_type,
+        'GORDON_APP_NAME'             => options.app_name,
+        'GORDON_APP_DESCRIPTION'      => options.app_description,
+        'GORDON_APP_HOMEPAGE'         => options.app_homepage,
+        'GORDON_APP_VERSION'          => options.app_version,
+        'GORDON_APP_SOURCE'           => options.app_source,
+        'GORDON_APP_SOURCE_EXCLUDES'  => options.app_source_excludes.join(","),
+        'GORDON_RUNTIME_NAME'         => options.runtime_name,
+        'GORDON_RUNTIME_VERSION'      => options.runtime_version,
+        'GORDON_HTTP_SERVER_TYPE'     => options.http_server_type,
+        'GORDON_WEB_SERVER_TYPE'      => options.web_server_type,
+        'GORDON_INIT_TYPE'            => options.init_type,
       }
 
       env.each_pair { |k, v| expect(ENV).to receive(:[]).with(k).and_return(v.to_s) }
 
       described_class.load.tap do |env_vars|
         env.each_pair do |k, v|
-          expect(env_vars.send(k.gsub(/GORDON_/, '').downcase)).to eq(v.to_s)
+          expected = k == 'GORDON_APP_SOURCE_EXCLUDES' ? options.app_source_excludes : v.to_s
+
+          expect(env_vars.send(k.gsub(/GORDON_/, '').downcase)).to eq(expected)
         end
       end
     end
